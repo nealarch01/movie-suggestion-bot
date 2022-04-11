e5f4ea03f4b9ee7ccd525d376aea0db8f4cd1261
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-11 12:56:18 -0700

    API call to convert a movie name into tmdb movie ID

---
+++ src/utils/movie-id.ts
@@ -0,0 +1,24 @@
+// Library imports
+import axios from "axios";
+
+// JSON imports
+import { tmdb_api_key } from "../../client_config.json";
+
+// this function will return the id of the top result if there are multiple
+async function getMovieID(movie_input: string): Promise<number> {
+    // documentation link for search: https://developers.themoviedb.org/3/search/search-companies
+    try {
+        let searchURL = `https://api.themoviedb.org/3/search/movie?api_key=${tmdb_api_key}&language=en-US&query=${movie_input}&page=1&include_adult=false`;
+        let response = await axios.get(searchURL);
+        if (response.data.total_results === 0) { // no serch results were entered
+            console.log("No results were found");
+            return -1;
+        }
+        return response.data.results[0].id;
+    } catch (err) {
+        console.log("There was an error getting movie data");
+        return -1; // return negative number to indicate there was an error obtaining movie data
+    }
+}
+
+export default getMovieID;
