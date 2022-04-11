1c7feae8364cf3d6dd6aff1c96ed0785b4f0b280
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-11 11:22:30 -0700

    Use total_results attribute instead of Array.length

--- src/random-movie.ts
+++ src/random-movie.ts
@@ -1,7 +1,10 @@
+// library imports
 import axios from "axios"
 
+// interface imports
 import IMovieData from "./interfaces/IMovieData";
 
+// json imports
 import { tmdb_api_key } from "../client_config.json";
 
 
@@ -23,7 +26,7 @@ const movieGenres: Array<string> = ["action",
     "horror",
     "western",
     "war"
-]
+];
 
 
 const genreMap: Map<string, string | undefined> = new Map();
@@ -65,15 +68,16 @@ async function getRandomMovie(genreInput: string): Promise<IMovieData> {
     discoverURL = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb_api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${generatePageNumber()}&with_genres=${genreID}`;
     try {
         let response = await axios.get(discoverURL); // queries an entire list of movies of the specified genre
-        if (response.data.results.length === 0) { // case for if the random page number generated does not contain any results
+        if (response.data.total_results === 0) { // case for if the random page number generated does not contain any results
             discoverURL = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb_api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${1}&with_genres=${genreID}`;
             response = await axios.get(discoverURL);
         }
 
         // if no data was found again in the first page, throw an error and return unsuccessful
-        if (response.data.results.length === 0) {
+        if (response.data.total_results === 0) {
             throw "No data found";
         }
+        
         const moviesList: Array<object> = response.data.results;
 
         const randomMovieData: any = moviesList[Math.floor(Math.random() * moviesList.length)];
