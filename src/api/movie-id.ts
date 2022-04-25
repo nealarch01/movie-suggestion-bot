91436a1bbb5f52406360bb5bbcaee8b6f3999363
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-24 18:38:02 -0700

    Removed console log when no results are found

--- src/api/movie-id.ts
+++ src/api/movie-id.ts
@@ -10,13 +10,13 @@ async function getMovieID(movie_input: string): Promise<number> {
     try {
         let searchURL = `https://api.themoviedb.org/3/search/movie?api_key=${tmdb_api_key}&language=en-US&query=${movie_input}&page=1&include_adult=false`;
         let response = await axios.get(searchURL);
-        if (response.data.total_results === 0) { // no serch results were entered
-            console.log("No results were found");
+        if (response.data.total_results === 0) { // no serch results were found
+            // console.log("No results were found");
             return -1;
         }
         return response.data.results[0].id;
     } catch (err) {
-        console.log("There was an error getting movie data");
+        console.log("There was an error getting movie data (movie-id.ts)");
         return -1; // return negative number to indicate there was an error obtaining movie data
     }
 }
