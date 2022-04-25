dfede22048c6c3b4928d9c877d06a12a0d12b94d
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-24 18:40:48 -0700

    Reject command if a genre not part of array is entered

--- src/commands/random-movie-command.ts
+++ src/commands/random-movie-command.ts
@@ -10,15 +10,40 @@ import IMovieData from "../interfaces/IMovieData";
 // Utility import
 import formatMovieMessage from "../utils/format-movie-message";
 
+const validGenres: Array<string> = ["action",
+    "adventure",
+    "animation",
+    "comedy",
+    "crime",
+    "drama",
+    "documentary",
+    "fantasy",
+    "family",
+    "music",
+    "musicals",
+    "historical",
+    "romance",
+    "sci-fi",
+    "thriller",
+    "horror",
+    "western",
+    "war"
+];
+
 async function randomMovieCommand(interaction: CommandInteraction): Promise<void> {
     const commandArguments = interaction.options;
     let specifiedGenre: string = "";
-    if (commandArguments.data.length > 0) {
-        // generate a random movie of the specified genre
-        if (typeof(commandArguments.data[0].value) === "string") {
+    if (commandArguments.data.length > 0) { // There is a parameter (genre input)
+        if (typeof(commandArguments.data[0].value) === "string") { 
             specifiedGenre = commandArguments.data[0].value;
+            if (!validGenres.includes(specifiedGenre.toLowerCase())) { // Check if a valid genre is entered
+                // Reject invalid genre input
+                await interaction.reply(`Invalid genre.\nThe valid genres are: ${ await genreTypesMessage() }`);
+                return;
+            }
         }
     }
+    // If a genre is not specified, then an empty string is passed
     let generatedMovie: IMovieData = await getRandomMovie(specifiedGenre);
     if (generatedMovie.success === true) {
         await interaction.reply(`${formatMovieMessage(generatedMovie)}`);
@@ -26,5 +51,20 @@ async function randomMovieCommand(interaction: CommandInteraction): Promise<void
         await interaction.reply("There was an error obtaining movie data");
     }
 }
+
+async function genreTypesMessage(): Promise<string> {
+    let validGenresMessage: string = "";
+    validGenres.forEach((element: string, index: number) => {
+        validGenresMessage += `${element}`
+        if (index === validGenres.length - 1) {
+            validGenresMessage += '.';
+        } else {
+            validGenresMessage += ', ';
+        }
+    });
+    return validGenresMessage;
+}
+
+
 
 export default randomMovieCommand;
