911b78463783e4cd51ac9500409cfc8f0ca85ae2
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-05-04 22:46:01 -0700

    Remove tmdb rating as requested

--- src/utils/format-movie-message.ts
+++ src/utils/format-movie-message.ts
@@ -10,7 +10,6 @@ ${attributeSource_tmdb("")}
 **${movieData.movie_name}**
 **Released**: ${movieData.movie_release_date}
 **Description**: ${movieData.movie_description}
-**${movieData.movie_user_ratings}%** liked this movie (tmdb)
 ${(movieData.poster_URL === null) ? "No poster to display" : `[poster](https://image.tmdb.org/t/p/w1280${movieData.poster_URL})`}
 `;
 }
