09e7cf8db83b4ccfc7419dd6ff2993b672264627
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-11-11 15:16:25 -0800

    Add command registration on bot join

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -2,50 +2,30 @@
 // However, if changes are made, then rerunning this is necessary
 // Script has already been defined as: npm run deploy-commands
 
-import { SlashCommandBuilder } from "@discordjs/builders";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord.js/node_modules/discord-api-types/v9";
 import { discord_token, discord_clientID, guild_id } from './client_config.json';
+import movieBotCommands from './bot-commands';
 
-const allGenres: Array<[string, string]> = [['Horror', 'Scary movies'],
-                                            ['Comedy', 'Funny movies'],
-                                            ['Thriller', 'Thrilling movies'],
-                                            ['Action', 'Action movies'],
-                                            ['Adventure', 'Adventorous movies'],
-                                            ['Animation', 'Animated movies'],
-                                            ['History', 'Historical movies'],
-                                            ['Documentary', 'Documentary films'],
-                                            ['Music', 'Musicals'],
-                                            ['Mystery', 'Mystery movies'],
-                                            ['Romance', 'Romantic movies'],
-                                            ['Sci-fi', 'Science fiction movies'],
-                                            ['Drama', 'Drama movies'],
-                                            ['War', 'War movies'],
-                                            ['Crime', 'True crime movies'],
-                                            ['Family', 'Family friendly movies']];
+// Manual import of commands
+// This is not part of the main program
 
-// registering bot commands through an array of SlashCommandBuilders
-const movieBotCommands = [
-    // returns a random movie title
-    new SlashCommandBuilder()
-        .setName('movie-random')
-        .setDescription('Gives you a random movie suggestion')
-        .addStringOption(movieGenre => movieGenre
-            .setName('genre')
-            .setRequired(false)
-            .setDescription('specify a genre of movie to generate')),
-    // returns a link to the movie trailer of a movie entered
-    new SlashCommandBuilder()
-        .setName('movie-trailer')
-        .setDescription('Sends a link of the movie trailer')
-        .addStringOption(movieName => movieName
-            .setName('name')
-            .setRequired(true)
-            .setDescription('name of movie'))
-];
+async function deployCommands_Manual() {
+    const rest = new REST({ version: '9' }).setToken(discord_token);
+    rest.put(Routes.applicationGuildCommands(discord_clientID, guild_id), { body: movieBotCommands })
+        .then(() => console.log('Commands successfully registered'))
+        .catch((err) => console.log(err, "\nAn error has occured registering commands"));
+}
 
-const rest = new REST({ version: '9' }).setToken(discord_token);
+async function deployCommands(guildId: string) {
+    const rest = new REST({ version: '9' }).setToken(discord_token);
+    rest.put(Routes.applicationGuildCommands(discord_clientID, guildId), { body: movieBotCommands })
+        .then(() => {
+            console.log('Commands successfully registered');
+        })
+        .catch((err) => {
+            console.log(err, "\nAn error has occured registering commands");
+        });
+}
 
-rest.put(Routes.applicationGuildCommands(discord_clientID, guild_id), { body: movieBotCommands })
-    .then(() => console.log('Commands successfully registered'))
-    .catch((err) => console.log(err, "\nAn error has occured registering commands"));
+export { deployCommands, deployCommands_Manual };
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 // Discord imports
-import { Client, Intents, Interaction } from "discord.js";
+import { Client, Guild, Intents, Interaction } from "discord.js";
 import { discord_token } from "./client_config.json";
+
+// Bot commands import
+import { deployCommands } from "./deploy-commands";
 
 // Command handlers
 import randomMovieCommand from "./src/commands/random-movie-command";
@@ -29,6 +32,11 @@ discordClient.on("interactionCreate", async (interaction: Interaction) => {
     } else if (commandSent === "movie-trailer") {
         await movieTrailerCommand(interaction);
     }
+});
+
+// GuildCreate event is emitted when the bot joins a new guild (server)
+discordClient.on("guildCreate", async (guild: Guild) => {
+    await deployCommands(guild.id); 
 });
 
 // log into client passing api token
